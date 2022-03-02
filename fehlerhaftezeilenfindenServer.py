5e01f960095144c66d8e609db3ee0387a74d8804
Kyra <kyra.sebestyen@pludoni.de>
2022-03-02 10:02:06 +0100

    Chunk 1 kategorisieren

--- fehlerhaftezeilenfindenServer.py
+++ fehlerhaftezeilenfindenServer.py
@@ -71,12 +71,12 @@ def categorize(corpusChunk):
         if not found:
             notFound.append(line)
 
-    pandas.DataFrame(valid).to_csv('/disk2/ksebestyen/Valid0.csv', sep=';', quoting=3)
-    pandas.DataFrame(mwu).to_csv('/disk2/ksebestyen/MWU0.csv', sep=';', quoting=3)
-    pandas.DataFrame(notFound).to_csv('/disk2/ksebestyen/NotFound0.csv', sep=';', quoting=3)
+    pandas.DataFrame(valid).to_csv('/disk2/ksebestyen/Valid1.csv', sep=';', quoting=3)
+    pandas.DataFrame(mwu).to_csv('/disk2/ksebestyen/MWU1.csv', sep=';', quoting=3)
+    pandas.DataFrame(notFound).to_csv('/disk2/ksebestyen/NotFound1.csv', sep=';', quoting=3)
 
 
-corpus = pandas.read_csv('/disk2/ksebestyen/ChunkList0.csv', sep=';', quoting=3, dtype='str')  # 3 means QUOTE_NONE
+corpus = pandas.read_csv('/disk2/ksebestyen/ChunkList1.csv', sep=';', quoting=3, dtype='str')  # 3 means QUOTE_NONE
 corpus['Sentence'] = corpus['Sentence'].astype(str)
 corpus = corpus[corpus.Sentence.map(len) < 1000]
 
