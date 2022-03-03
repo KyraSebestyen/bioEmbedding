5006b04bdaac897aa6279101ac352ea0a51cd12f
Kyra <kyra.sebestyen@pludoni.de>
2022-03-03 17:29:47 +0100

    valid corpus chunks zu einem Korpus zusammenfügen

---
+++ combineValidCorpusChunks.py
@@ -0,0 +1,13 @@
+import glob
+import pandas
+# get data file names
+path = '/disk2/ksebestyen/Valid'
+filenames = glob.glob(path + "/*.csv")
+
+corpusChunks = []
+for filename in filenames:
+    corpusChunks.append(pandas.read_csv(filename, sep=';', quoting=3))
+
+# Concatenate all data into one DataFrame
+validCorpus = pandas.concat(corpusChunks, ignore_index=True)
+validCorpus.to_csv('/disk2/ksebestyen/validCorpus.csv', sep=';', quoting=3)
