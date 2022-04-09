46e20b928d3b486accee5d3207d42f82991e77be
Kyra <kyra.sebestyen@pludoni.de>
2022-04-09 12:30:05 +0200

    db auslesen test

--- elbowMethod.py
+++ elbowMethod.py
@@ -8,11 +8,15 @@ import sqlite3
 embed_db = sqlite3.connect('/disk2/ksebestyen/embed_db.db', detect_types=sqlite3.PARSE_DECLTYPES)
 cursor = embed_db.cursor()
 
-dataFromDB = cursor.execute("SELECT token_id, embedding FROM embeddings WHERE pos_univ IN ('ADJ', 'NOUN', 'PROPN')").fetchall()
+dataFromDB = cursor.execute("SELECT token_id, embedding FROM embeddings WHERE pos_univ IN ('ADJ', 'NOUN', 'PROPN') limit 10").fetchall()
 dataFromDBDataFrame = pandas.DataFrame(dataFromDB)
 print(dataFromDBDataFrame.head())
 
-'''from sklearn import preprocessing  # to normalise existing X
+dataFromDBDataFrame["embedding"] = [numpy.frombuffer(entry) for entry in dataFromDBDataFrame["embedding"]]
+print(dataFromDBDataFrame.head())
+
+'''
+from sklearn import preprocessing  # to normalise existing X
 X_Norm = preprocessing.normalize(X)
 
 km2 = cluster.KMeans(n_clusters=5,init='random').fit(X_Norm)
