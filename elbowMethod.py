b1aec195ed70bd4b414449eb7c21b7ea4ec63f0d
Kyra <kyra.sebestyen@pludoni.de>
2022-04-09 12:00:53 +0200

    db auslesen test

---
+++ elbowMethod.py
@@ -0,0 +1,22 @@
+import pandas
+import numpy
+import matplotlib.pyplot as plt
+%matplotlib inline
+from sklearn.cluster import KMeans
+from sklearn import datasets
+
+embed_db = sqlite3.connect('/disk2/ksebestyen/embed_db.db', detect_types=sqlite3.PARSE_DECLTYPES)
+cursor = embed_db.cursor()
+
+dataFromDB = cursor.execute("SELECT token_id, embedding FROM embeddings WHERE pos_univ IN ('ADJ', 'NOUN', 'PROPN')").fetchall()
+dataFromDBDataFrame = pandas.DataFrame(dataFromDB)
+dataFromDBDataFrame.head()
+
+'''
+distortions = []
+K = range(1,10)
+for k in K:
+    kmeanModel = KMeans(n_clusters=k)
+    kmeanModel.fit(dataFromDBDataFrame)
+    distortions.append(kmeanModel.inertia_)
+'''
