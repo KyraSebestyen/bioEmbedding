4cd439bcdd77eb61bf58aed2e24cb465f33ffefc
Kyra <kyra.sebestyen@pludoni.de>
2022-05-08 18:12:54 +0200

    test sql

--- combineMetadata.py
+++ combineMetadata.py
@@ -4,30 +4,31 @@ import sqlite3
 import pandas
 columns = ["workId", "Year"]
 metadataCSV = pandas.read_csv("/disk2/ksebestyen/extCompDB.csv", sep=",", usecols=columns)
-
-embed_db = sqlite3.connect('/disk2/ksebestyen/embed_db_with_file.db', detect_types=sqlite3.PARSE_DECLTYPES)
-cursor = embed_db.cursor()
-
-cursor.execute('''DROP TABLE IF EXISTS metadata''')
-
-sql_ = '''CREATE TABLE metadata (
-        workId TEXT,
-        year INTEGER
-)'''
-cursor.execute(sql_)
+#
+# embed_db = sqlite3.connect('/disk2/ksebestyen/embed_db_with_file.db', detect_types=sqlite3.PARSE_DECLTYPES)
+# cursor = embed_db.cursor()
+#
+# cursor.execute('''DROP TABLE IF EXISTS metadata''')
+#
+# sql_ = '''CREATE TABLE metadata (
+#         workId TEXT,
+#         year INTEGER
+# )'''
+# cursor.execute(sql_)
 
 for idx in metadataCSV.index:
     data = metadataCSV.loc[idx, :].values.tolist()
-    sql_ = '''INSERT INTO metadata values (?, ?)'''
-    cursor.execute(sql_, tuple([data[0], data[1]]))
+    print(data)
+#     sql_ = '''INSERT INTO metadata values (?, ?)'''
+#     cursor.execute(sql_, tuple([data[0], data[1]]))
+#
+# embed_db.commit()
+# print("embeddings saved in database")
 
-embed_db.commit()
-print("embeddings saved in database")
-
-for column in ["workId", "year"]:
-    sql_ = f'''CREATE INDEX {column} on metadata ({column})'''
-    cursor.execute(sql_)
-
-embed_db.commit()
-
-embed_db.close()
+# for column in ["workId", "year"]:
+#     sql_ = f'''CREATE INDEX {column} on metadata ({column})'''
+#     cursor.execute(sql_)
+#
+# embed_db.commit()
+#
+# embed_db.close()
